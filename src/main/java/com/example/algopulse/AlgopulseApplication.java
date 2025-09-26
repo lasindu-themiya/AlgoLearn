0d1a628c43996c08b93e2782ff15ff75df5e7792
Anuththara H L L T <lasinduthemiya96@gmail.com>
2025-09-26 09:46:27 +0530

    initialized gitignore

---
+++ src/main/java/com/example/algopulse/AlgopulseApplication.java
@@ -0,0 +1,13 @@
+package com.example.algopulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgopulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgopulseApplication.class, args);
+	}
+
+}
